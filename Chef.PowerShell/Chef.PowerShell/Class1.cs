eef21a5f98a8ddeb3ad837b9102adfb4b7b69d85
Matt Wrock <matt@mattwrock.com>
2015-10-16 16:21:50 -0700

    initial commit

---
+++ Chef.PowerShell/Chef.PowerShell/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+
+namespace Chef.PowerShell
+{
+    public static class RunspaceExecutor
+    {
+        private static System.Management.Automation.Runspaces.RunspacePool rp;
+        private static System.Management.Automation.PowerShell ps;
+
+        static RunspaceExecutor()
+        {
+            rp = System.Management.Automation.Runspaces.RunspaceFactory.CreateRunspacePool();
+            ps = System.Management.Automation.PowerShell.Create();
+
+            rp.Open();
+            ps.RunspacePool = rp;
+        }
+
+        public static int ExecutePs(string input)
+        {
+            var script = "'" + input + "'.length";
+            ps.AddScript(script);
+            var result = ps.Invoke().First().BaseObject;
+            int number = (result as int?).Value;
+            return number;
+        }
+    }
+}
